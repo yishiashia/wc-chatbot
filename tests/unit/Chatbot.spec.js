016a49d8245e5d691efe50883fe1a22c2f6e4004
yishiashia <yishiashia@gmail.com>
2023-11-03 10:08:59 +0800

    chore: upgrade wc-bubble to version 0.1.5

--- tests/unit/Chatbot.spec.js
+++ tests/unit/Chatbot.spec.js
@@ -1,15 +1,18 @@
-import { fireEvent } from '@testing-library/dom';
-import '@testing-library/jest-dom'
+import { fireEvent } from "@testing-library/dom";
+import "@testing-library/jest-dom";
 
-import Chatbot from '../../src/Chatbot';
+import Chatbot from "../../src/Chatbot";
 
-window.customElements.define('chat-bot', Chatbot)
+window.customElements.define("chat-bot", Chatbot);
 
-describe('Chatbot web component', () => {
+// Mock the replaceSync() method
+global.CSSStyleSheet.prototype.replaceSync = jest.fn();
+
+describe("Chatbot web component", () => {
   let chatbot;
 
   beforeEach(() => {
-    chatbot = document.createElement('chat-bot');
+    chatbot = document.createElement("chat-bot");
     document.body.appendChild(chatbot);
     // jest.spyOn(Element, 'scrollTo')
   });
@@ -18,105 +21,111 @@ describe('Chatbot web component', () => {
     document.body.removeChild(chatbot);
   });
 
-  test('renders the chatbot component', () => {
+  test("renders the chatbot component", () => {
     expect(chatbot).toBeInTheDocument();
   });
 
-  test('renders a header', () => {
-    const header = chatbot.shadowRoot.querySelector('.chatbot-header');
+  test("renders a header", () => {
+    const header = chatbot.shadowRoot.querySelector(".chatbot-header");
     expect(header).not.toBeNull();
   });
 
-  test('renders a footer', () => {
-    const footer = chatbot.shadowRoot.querySelector('.chat-footer');
+  test("renders a footer", () => {
+    const footer = chatbot.shadowRoot.querySelector(".chat-footer");
     expect(footer).not.toBeNull();
   });
 
-  test('renders an avatar button', () => {
-    const avatarBtn = chatbot.shadowRoot.querySelector('.bot-button.avatar');
+  test("renders an avatar button", () => {
+    const avatarBtn = chatbot.shadowRoot.querySelector(".bot-button.avatar");
     expect(avatarBtn).not.toBeNull();
   });
 
-  test('renders a text input', () => {
-    const textInput = chatbot.shadowRoot.querySelector('#msg-input');
+  test("renders a text input", () => {
+    const textInput = chatbot.shadowRoot.querySelector("#msg-input");
     expect(textInput).not.toBeNull();
   });
 
-  test('can send a message', () => {
-    chatbot.sendMessage('Hello');
-    const messages = chatbot.shadowRoot.querySelectorAll('chat-bubble');
+  test("can send a message", () => {
+    chatbot.sendMessage("Hello");
+    const messages = chatbot.shadowRoot.querySelectorAll("chat-bubble");
     expect(messages.length).toBe(1);
   });
 
-  test('send a message when form submit', async () => {
+  test("send a message when form submit", async () => {
     // const textInput = chatbot.shadowRoot.getElementById('msg-input')
-    const form = chatbot.shadowRoot.querySelector('form');
+    const form = chatbot.shadowRoot.querySelector("form");
     // textInput.value = 'hello'
-    form.msg = { value: "hellp"}
+    form.msg = { value: "hellp" };
     await fireEvent.submit(form);
-    const messages = chatbot.shadowRoot.querySelectorAll('chat-bubble');
+    const messages = chatbot.shadowRoot.querySelectorAll("chat-bubble");
     expect(messages.length).toBe(1);
   });
 
-  test('get messages from component', () => {
-    chatbot.sendMessage('Hello');
+  test("get messages from component", () => {
+    chatbot.sendMessage("Hello");
     const messages = chatbot.messages;
     expect(messages.length).toBe(1);
   });
 
-  test('scroll to bottom', async () => {
-    const dispatchEventSpy = jest.spyOn(chatbot, 'dispatchEvent');
-    const propertyDescriptor = Object.getOwnPropertyDescriptor(Element.prototype, 'scrollTo');
+  test("scroll to bottom", async () => {
+    const dispatchEventSpy = jest.spyOn(chatbot, "dispatchEvent");
+    const propertyDescriptor = Object.getOwnPropertyDescriptor(
+      Element.prototype,
+      "scrollTo"
+    );
     const spyScrollTo = jest.fn();
-    Object.defineProperty(chatbot.body, 'scrollTo', { value: spyScrollTo });
-    chatbot.sendMessage('Hello');
-    const messages = chatbot.shadowRoot.querySelectorAll('chat-bubble');
+    Object.defineProperty(chatbot.body, "scrollTo", { value: spyScrollTo });
+    chatbot.sendMessage("Hello");
+    const messages = chatbot.shadowRoot.querySelectorAll("chat-bubble");
     expect(messages.length).toBe(1);
-    await fireEvent.animationEnd(messages[0])
-    await fireEvent(messages[0], new CustomEvent("mounted"))
-    expect(dispatchEventSpy).toHaveBeenCalled()
+    await fireEvent.animationEnd(messages[0]);
+    await fireEvent(messages[0], new CustomEvent("mounted"));
+    expect(dispatchEventSpy).toHaveBeenCalled();
   });
 
-  test('title attribute', () => {
+  test("title attribute", () => {
     document.body.removeChild(chatbot);
-    const titleStr = "JustWe"
-    chatbot.setAttribute("title", titleStr)
-    document.body.appendChild(chatbot)
-    const titleElement = chatbot.shadowRoot.querySelector('.chatbot-header h3');
+    const titleStr = "JustWe";
+    chatbot.setAttribute("title", titleStr);
+    document.body.appendChild(chatbot);
+    const titleElement = chatbot.shadowRoot.querySelector(".chatbot-header h3");
     expect(titleElement.textContent).toBe(titleStr);
   });
 
-  test('send HTML message', () => {
-    chatbot.sendMessage(null, { message: '<p id="test-p">hello <a id="test-a" href="http://example.org">123</a></p>' });
-    const aElement = chatbot.shadowRoot.querySelector('#test-a');
-    const pElement = chatbot.shadowRoot.querySelector('#test-p');
+  test("send HTML message", () => {
+    chatbot.sendMessage(null, {
+      message:
+        '<p id="test-p">hello <a id="test-a" href="http://example.org">123</a></p>',
+    });
+    const aElement = chatbot.shadowRoot.querySelector("#test-a");
+    const pElement = chatbot.shadowRoot.querySelector("#test-p");
     expect(aElement).not.toBeNull();
     expect(pElement).not.toBeNull();
-  })
+  });
 
-  test('send continued message', () => {
-    chatbot.sendMessage('Hello');
-    chatbot.sendMessage('Hello');
-    const messages = chatbot.shadowRoot.querySelectorAll('chat-bubble');
+  test("send continued message", () => {
+    chatbot.sendMessage("Hello");
+    chatbot.sendMessage("Hello");
+    const messages = chatbot.shadowRoot.querySelectorAll("chat-bubble");
     expect(messages.length).toBe(2);
     expect(messages[1].continued).toBe(true);
   });
 
-  test('can toggle the chatbot', async () => {
-    const container = chatbot.shadowRoot.querySelector('.chatbot-container');
-    expect(container.classList.contains('hidden')).toBe(true);
+  test("can toggle the chatbot", async () => {
+    const container = chatbot.shadowRoot.querySelector(".chatbot-container");
+    expect(container.classList.contains("hidden")).toBe(true);
     chatbot.toggle();
-    expect(container.classList.contains('hidden')).toBe(false);
-    expect(container.classList.contains('animation-scale-out')).toBe(false);
-    expect(container.classList.contains('animation-scale-in')).toBe(true);
-    await fireEvent.animationEnd(chatbot.container)
+    expect(container.classList.contains("hidden")).toBe(false);
+    expect(container.classList.contains("animation-scale-out")).toBe(false);
+    expect(container.classList.contains("animation-scale-in")).toBe(true);
+    await fireEvent.animationEnd(chatbot.container);
     chatbot.toggle();
-    expect(container.classList.contains('hidden')).toBe(false);
-    expect(container.classList.contains('animation-scale-out')).toBe(true);
-    expect(container.classList.contains('animation-scale-in')).toBe(false);
+    expect(container.classList.contains("hidden")).toBe(false);
+    expect(container.classList.contains("animation-scale-out")).toBe(true);
+    expect(container.classList.contains("animation-scale-in")).toBe(false);
     chatbot.toggle();
-    expect(container.classList.contains('hidden')).toBe(false);
-    expect(container.classList.contains('animation-scale-out')).toBe(false);
-    expect(container.classList.contains('animation-scale-in')).toBe(true);
+    expect(container.classList.contains("hidden")).toBe(false);
+    expect(container.classList.contains("animation-scale-out")).toBe(false);
+    expect(container.classList.contains("animation-scale-in")).toBe(true);
   });
 });
